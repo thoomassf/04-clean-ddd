7a262599ef7ce7fb447fb9c3fa182472906595e7
Thomas Farias <thomasfarias19@gmail.com>
2024-04-21 17:34:04 -0300

    create get question by slug use-case and test

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -4,6 +4,16 @@ import { Question } from '@/domain/forum/enterprise/entities/question'
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
 
+  async findBySlug(slyg: string) {
+    const question = this.items.find((item) => item.slug.value === slyg)
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
   async create(question: Question) {
     this.items.push(question)
   }
